73824f1c2609c92042f6c0fde823f54d9bd993ae
julio1up <julio.gjyzeli@1up.al>
2023-02-23 14:43:20 -0800

    #7 t3

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,15 +4,13 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     '''Gettin Cozy'''
-    new_matrix = []
     if axis == 0:
         if len(mat1[0]) != len(mat2[0]):
             return None
-        new_matrix = mat1 + mat2
+        return mat1 + mat2
     elif axis == 1:
         if len(mat1) != len(mat1):
             return None
-        new_matrix = [mat1[i]+mat2[i] for i in range(len(mat1))]
+        return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
     else:
         return None
-    return new_matrix
