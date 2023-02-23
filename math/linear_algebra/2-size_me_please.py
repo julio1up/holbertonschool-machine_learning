c2a1bbcabf07e52cc04bf8cf70a723f3134cfe74
julio1up <julio.gjyzeli@1up.al>
2023-02-23 06:52:33 -0800

    #.2 8

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -10,7 +10,7 @@ def matrix_shape(matrix):
         matrix = matrix[0]
     return result
 
-    
+
 mat1 = [[1, 2], [3, 4]]
 (matrix_shape(mat1))
 mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
