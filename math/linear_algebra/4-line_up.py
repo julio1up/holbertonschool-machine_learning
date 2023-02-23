a43dbdec4d153d3fd1d5de1b55a59be668b8f485
julio1up <julio.gjyzeli@1up.al>
2023-02-23 08:12:10 -0800

    #4 p1

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''Line Up Function'''
+
+def add_arrays(vec1, vec2):
+    '''Line Up Function'''
+    result = []
+    if(len(vec1) != len(vec2)):
+        return None
+    for i in range(len(vec1)):
+        result.append(vec1[i] + vec2[i])
+    return result
+
+
+arr1 = [1, 2, 3, 4]
+arr2 = [5, 6, 7, 8]
+add_arrays(arr1, arr2)
+add_arrays(arr1, [1, 2, 3])
