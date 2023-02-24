2e64d923fbba75e48e20c31a10f87609d426425a
julio1up <julio.gjyzeli@1up.al>
2023-02-24 02:18:54 -0800

    #10 t1

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''I'll Use My Scale Function '''
+
+
+import numpy as np
+def np_shape(matrix):
+        '''I'll Use My Scale Function '''
+            return matrix.shape
+
+
+mat1 = np.array([1, 2, 3, 4, 5, 6])
+mat2 = np.array([])
+mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+                         [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
