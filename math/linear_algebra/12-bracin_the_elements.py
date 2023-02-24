a80f5a9c2e8f3e4c2241ef594715ba28050be738
julio1up <julio.gjyzeli@1up.al>
2023-02-24 03:02:52 -0800

    #12 t5

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -5,9 +5,8 @@
 
 def np_elementwise(mat1, mat2):
     '''Bracing Elements Function'''
-    np
-    add = np.add(mat1, mat2)
-    sub = np.subtract(mat1, mat2)
-    mul = np.multiply(mat1, mat2)
-    div = np.divide(mat1, mat2)
+    add = add(mat1, mat2)
+    sub = subtract(mat1, mat2)
+    mul = multiply(mat1, mat2)
+    div = divide(mat1, mat2)
     return (add, sub, mul, div)
