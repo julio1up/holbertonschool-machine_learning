a5eb8679c7435b31c5357198c664d1089078df4b
julio1up <julio.gjyzeli@1up.al>
2023-02-24 03:00:38 -0800

    #12 t4

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -2,9 +2,10 @@
 '''Bracing Elements Function'''
 
 
-np
+
 def np_elementwise(mat1, mat2):
     '''Bracing Elements Function'''
+    np
     add = np.add(mat1, mat2)
     sub = np.subtract(mat1, mat2)
     mul = np.multiply(mat1, mat2)
