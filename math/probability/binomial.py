a8de86efa620bd65c4070bb03b19aea03fd23b93
julio1up <julio.gjyzeli@1up.al>
2023-03-14 04:40:56 -0700

    Fix pycodestyle  binomial

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -23,7 +23,7 @@ class Binomial:
             self.p = 1 - ((var) / mean)
             self.n = int(round(mean / self.p))
             self.p = mean / self.n
- 
+
     def pmf(self, k):
         """Probability Mass Function for binomial"""
         if type(k) is not int:
