f1313bce88a584a7b7ed9d440f8f7711e4832dd9
julio1up <julio.gjyzeli@1up.al>
2023-03-21 04:17:08 -0700

    Fix #4

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -4,12 +4,13 @@
 
 import numpy as np
 
-def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+
+def convolve_channels(images, kernel, padding="same", stride=(1, 1)):
     """Performs a convolution on images with channels"""
     m, height, width, c = images.shape
-    kh, kw = kernel.shape
+    kh, kw, c = kernel.shape
     sh, sw = stride
-    
+
     if padding == "same":
         ph = ((height - 1) * sh + kh - height) // 2 + 1
         pw = ((width - 1) * sw + kw - width) // 2 + 1
@@ -18,18 +19,18 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
         pw = 0
     else:
         ph, pw = padding
-    
-    p_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0 )))
-    
-    ch = ((height + 2*ph - kh) // sh) + 1
-    cw = ((width + 2*pw - kw) // sw) + 1
-    
+
+    p_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)))
+
+    ch = ((height + 2 * ph - kh) // sh) + 1
+    cw = ((width + 2 * pw - kw) // sw) + 1
+
     convoluted = np.zeros((m, ch, cw))
-    
+
     for h in range(ch):
         for w in range(cw):
-            output = p_images[:, h *sh: h * sh + kh,
+            output = p_images[:, h * sh: h * sh + kh, 
                               w * sw: w * sw + kw] * kernel
-            sum_out = np.sum(output, axis = (1, 2, 3))
+            sum_out = np.sum(output, axis=(1, 2, 3))
             convoluted[:, h, w] = sum_out
     return convoluted
