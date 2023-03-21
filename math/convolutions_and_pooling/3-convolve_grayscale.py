80a87d295109eeda914e13d679169faf19bdc467
julio1up <julio.gjyzeli@1up.al>
2023-03-21 03:51:08 -0700

    Fix check #3

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -13,11 +13,8 @@ def convolve_grayscale(images, kernel, padding="same", stride=(1, 1)):
     sh, sw = stride
 
     if padding == "same":
-        if kh % 2 == 0 and kw % 2 == 0:
-            ph = (kh - 1) // 2
-            pw = (kw - 1) // 2
-        ph = kh // 2
-        pw = kw // 2
+        ph = ((height - 1) * sh + kh - height) // 2 + 1
+        pw = ((width - 1) * sw + kw - width) // 2 + 1
     elif padding == "valid":
         ph = 0
         pw = 0
@@ -31,13 +28,10 @@ def convolve_grayscale(images, kernel, padding="same", stride=(1, 1)):
 
     convoluted = np.zeros((m, ch, cw))
 
-    i = 0
-    for h in range(0, height + 2 * ph - kh + 1, sh):
-        j = 0
-        for w in range(0, width + 2 * pw - kw + 1, sw):
-            output = p_images[:, h: h + kh, w: w + kw] * kernel
+    for h in range(ch):
+        for w in range(cw):
+            output = p_images[:, h * sh : h * sh + kh, 
+                              w * sw : w * sw + kw] * kernel
             sum_out = np.sum(output, axis=(1, 2))
-            convoluted[:, i, j] = sum_out
-            j += 1
-        i += 1
+            convoluted[:, h, w] = sum_out
     return convoluted
