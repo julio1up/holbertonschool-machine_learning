3d12f944880c07b0e80c747b9c1f6e703664f252
julio1up <julio.gjyzeli@1up.al>
2023-03-25 05:29:39 -0700

    Fix #1

--- math/multivariate_prob/1-correlation.py
+++ math/multivariate_prob/1-correlation.py
@@ -10,7 +10,7 @@ def correlation(C):
     Calculates a correlation matrix
     """
     if not isinstance(C, np.ndarray):
-        raise TypeError("C must be a 2D numpy.ndarray")
+        raise TypeError("C must be a numpy.ndarray")
     if (len(C.shape) != 2) or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
     # here's how you can calculate the square root of the diagonal matrix
