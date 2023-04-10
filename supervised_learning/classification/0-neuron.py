e56ff3b5eb4411ef3459b1992293bbcf45a2651a
julio1up <julio.gjyzeli@1up.al>
2023-04-10 08:33:54 -0700

    Fix #0 v1

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -10,7 +10,6 @@ class Neuron:
     class that represents a single neuron performing binary classification
     """
 
-
     def __init__(self, nx):
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
