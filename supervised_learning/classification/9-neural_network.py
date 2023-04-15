2dc57a8677cda86ee38056df4cfedb79d2cf4bdb
julio1up <julio.gjyzeli@1up.al>
2023-04-15 04:00:42 -0700

    Fix #9

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -11,9 +11,10 @@ class NeuralNetwork:
     performing binary classification
     """
 
+
     def __init__(self, nx, nodes):
         """
-        Class constructor
+        class constructor
         """
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
@@ -23,18 +24,14 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
-
-        # nodes number of nodes and receives nx number of input feature
-        self.W1 = np.random.randn(nodes, nx)
-        # matches the shape of the hidden layer output
-        self.b1 = np.zeros((nodes, 1))
-        self.A1 = 0
-
-        # only one node and receives the output from the hidden layer as input
-        self.W2 = np.random.randn(1, nodes)
-        # matching the shape of the output neuron's activation
-        self.b2 = 0
-        self.A2 = 0
+        
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
 
     @property
     def W1(self):
