cd519f0d52bbc146618a3b0a30826cdc8b11f994
julio1up <julio.gjyzeli@1up.al>
2023-04-15 04:34:13 -0700

    Fix #9 Add #11

--- supervised_learning/classification/11-neural_network.py
+++ supervised_learning/classification/11-neural_network.py
@@ -55,3 +55,29 @@ class NeuralNetwork:
     @property
     def A2(self):
         return (self.__A2)
+
+    def forward_prop(self, X):
+        """
+        calculates the forward propagation of the neural network
+        """
+
+        z1 = np.matmul(self.W1, X) + self.b1
+        self.__A1 = 1 / (1 + (np.exp(-z1)))
+
+        z2 = np.matmul(self.W2, self.__A1) + self.b2
+        self.__A2 = 1 / (1 + (np.exp(-z2)))
+
+        return (self.A1, self.A2)
+
+    def cost(self, Y, A):
+        """
+        calculates the cost of the model using logistic regression
+        In binary classification, the output layer has a single node 
+        that represents the probability of the positive class. 
+        Thus, the cost function is calculated based on the output of this node 
+        (i.e., A) and the actual labels (i.e., Y).
+        """
+        m = Y.shape[1]
+        m_loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
+        cost = (1 / m) * (-(m_loss))
+        return (cost)
