113271b8904d45e21707d959d4defe69d03e1c0d
julio1up <julio.gjyzeli@1up.al>
2023-05-19 05:56:33 -0700

    Fix #12

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""A function that tests a neural network"""
+"""
+Tests a Neural Network
+"""
 import tensorflow.keras as K
 
 
 def test_model(network, data, labels, verbose=True):
-    """A function that tests a neural network"""
-    loss, accuracy = network.evaluate(x=data,
-                                      y=labels,
-                                      verbose=verbose)
-    return loss, accuracy
+    """
+    a function that tests a NN
+    """
+    return network.evaluate(data, labels, verbose=verbose)
