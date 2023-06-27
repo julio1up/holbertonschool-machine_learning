712a4cf41158416eb67a8f5844ac2cece4e2ea7d
julio1up <julio.gjyzeli@1up.al>
2023-06-27 05:16:31 -0700

    Yolo update

--- supervised_learning/object_detection/6-yolo.py
+++ supervised_learning/object_detection/6-yolo.py
@@ -1,57 +1,60 @@
 #!/usr/bin/env python3
 """
-YOLO(You Only Look Once !!!)
+   Module contains:
+   Class Yolo
 """
+
+
+from numpy.core.defchararray import index
 from numpy.core.fromnumeric import searchsorted
 import tensorflow.keras as K
 import tensorflow.keras.backend as backend
 from numpy import concatenate as cat
 import tensorflow as tf
 import numpy as np
+import cv2
 
 
-class Yolo:
+class Yolo():
     """
-    YOLO V3
+       Yolo v3 class for performing object detection.
+
+       Public Instance Attributes
+            model: the Darknet Keras model
+            class_names: a list of the class names for the model
+            class_t: the box score threshold for the initial filtering step
+            nms_t: the IOU threshold for non-max suppression
+            anchors: the anchor boxes
     """
+
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """
-        Args:
-            model_path: is the path to where a Darknet Keras model is stored
-            classes_path: is the path to where the list of class names used for
-                  the Darknet model, listed in order of index, can be found
-            class_t: representing the box score threshold for the initial
-                     filtering step
-            nms_t: representing the IOU threshold for non-max suppression
-            anchors:(outputs,anchor_boxes,2)containing all of the anchor boxs
-                outputs: is the number of outputs (predictions)
-                         made by the Darknet model
-                anchor_boxes: number of anchor boxes used for each predic
-                2 => [anchor_box_width, anchor_box_height]
+           Init method for instanciating Yolo class.
+
+           Args:
+             model_path: path to Darknet keras model
+             classes_path: path to list of class names for
+               darknet model
+             class_t: float representing box score for initial
+               filtering step
+             nms_t: float representing IOU threshold for non-max
+               supression
+             anchors: numpy.ndarray - shape (outputs, anchor_boxes, 2)
+               containing all anchor boxes
+                 outputs: number of predictions made
+                 anchor_boxes: number of anchor boxes for each pred.
+                 2 => [anchor_box_width, anchor_box_height]
         """
-        # Loading model with Keras
         self.model = K.models.load_model(model_path)
-        # Character   Meaning
-        # 'r'   open for reading (default)
-        # 'w'   open for writing, truncating the file first
-        # 'x'   open for exclusive creation, failing if the file already exists
-        # 'a'   open for writing, appending to the end of the file if it exists
-        # 'b'   binary mode
-        # 't'   text mode (default)
-        # '+'   open a disk file for updating (reading and writing)
-        # '
-        # U'   universal newlines mode (deprecated)
         with open(classes_path, 'rt') as fd:
             self.class_names = fd.read().rstrip('\n').split('\n')
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
 
-    def sigmoid(self, number):
-        """
-        Sigmoid activation function
-        """
-        return 1 / (1 + np.exp(-number))
+    def sigmoid(self, arr):
+        """sigmoid activation function"""
+        return 1 / (1+np.exp(-1*arr))
 
     def process_outputs(self, outputs, image_size):
         """
@@ -60,6 +63,7 @@ class Yolo:
                for single image.
              image_size: numpy.ndarray - images original
                size (image_height, image_width)
+
            Return:
               tuple - (boxes, box_confidence, box_class_probs)
               boxes: numpy.ndarray - (grid_height, grid_width, anchorboxes, 4)
@@ -83,8 +87,12 @@ class Yolo:
             cy = np.arange(gridW).reshape(1, gridW)
             cy = np.repeat(cy, gridH, axis=0).T
 
-            cornersX.append(np.repeat(cx[..., np.newaxis], anchors, axis=2))
-            cornersY.append(np.repeat(cy[..., np.newaxis], anchors, axis=2))
+            cornersX.append(
+                np.repeat(cx[..., np.newaxis], anchors, axis=2)
+                )
+            cornersY.append(
+                np.repeat(cy[..., np.newaxis], anchors, axis=2)
+                )
             # box confidence and class probability activations
             box_confidence.append(self.sigmoid(output[..., 4:5]))
             class_probs.append(self.sigmoid(output[..., 5:]))
@@ -94,10 +102,18 @@ class Yolo:
 
         # Predicted boundary box
         for x, box in enumerate(boxes):
-            bx = ((self.sigmoid(box[..., 0])+cornersX[x])/outputs[x].shape[1])
-            by = ((self.sigmoid(box[..., 1])+cornersY[x])/outputs[x].shape[0])
-            bw = ((np.exp(box[..., 2])*self.anchors[x, :, 0])/inputW)
-            bh = ((np.exp(box[..., 3])*self.anchors[x, :, 1])/inputH)
+            bx = (
+                (self.sigmoid(box[..., 0])+cornersX[x])/outputs[x].shape[1]
+                )
+            by = (
+                (self.sigmoid(box[..., 1])+cornersY[x])/outputs[x].shape[0]
+                )
+            bw = (
+                (np.exp(box[..., 2])*self.anchors[x, :, 0])/inputW
+                )
+            bh = (
+                (np.exp(box[..., 3])*self.anchors[x, :, 1])/inputH
+                )
 
             # x1
             box[..., 0] = (bx - (bw * 0.5))*IW
@@ -122,6 +138,7 @@ class Yolo:
             box_class_probs: a list of numpy.ndarrays of shape
               (grid_height, grid_width, anchor_boxes, classes) containing the
               processed box class probabilities for each output, respectively
+
            Return:
             filtered_boxes: a numpy.ndarray of shape (?, 4) containing
               all of the filtered bounding boxes
@@ -157,6 +174,7 @@ class Yolo:
               number for the class that filtered_boxes predicts, respectively
             box_scores: a numpy.ndarray of shape (?) containing the box scores
               for each box in filtered_boxes, respectively
+
            Return:
             Tuple (box_predictions, predicted_box_class, predicted_box_scores)
             box_predictions: a numpy.ndarray of shape (?, 4) containing all of
@@ -202,3 +220,130 @@ class Yolo:
 
         return (sup_boxes, sup_classes, sup_scores)
 
+    @staticmethod
+    def load_images(folder_path):
+        """
+           Args:
+            folder_path: a string representing the path to the folder
+              holding all the images to load
+
+           Return:
+            images: a list of images as numpy.ndarrays
+            image_paths: a list of paths to the individual images in images
+        """
+        import os
+        image_paths = os.listdir(folder_path)
+        images, new_paths = [], []
+
+        for path in image_paths:
+            images.append(cv2.imread(folder_path+"/"+path))
+            new_paths.append(folder_path+"/"+path)
+
+        return images, new_paths
+
+    def preprocess_images(self, images):
+        """
+           Args:
+            images: a list of images as numpy.ndarrays
+
+           Return:
+            images: a numpy.ndarray of shape (ni, input_h, input_w, 3)
+              containing all of the preprocessed images
+                ni: the number of images that were preprocessed
+                input_h: the input height for the Darknet model
+                  Note - this can vary by model
+                input_w: the input width for the Darknet model
+                  Note - this can vary by model
+                3: number of color channels
+            image_shapes: a numpy.ndarray of shape (ni, 2) containing
+              the original height and width of the images
+                2 => (image_height, image_width)
+        """
+        inputW = self.model.input.shape[1].value
+        inputH = self.model.input.shape[2].value
+        pimages, image_sizes = None, None
+        channels = None
+        dims = (inputH, inputW)
+        Q = cv2.INTER_CUBIC
+
+        for img in images:
+            for ch in range(3):
+                # resize each channel individually
+                if channels is None:
+                    channels = cv2.resize(
+                        img[:, :, ch], dims, interpolation=Q
+                    )
+                    channels = channels[..., np.newaxis]
+                else:
+                    resized = cv2.resize(
+                        img[:, :, ch], dims, interpolation=Q
+                    )
+                    resized = resized[..., np.newaxis]
+                    channels = np.concatenate((channels, resized), axis=2)
+
+            # Stack resized images
+            if pimages is None:
+                pimages = (channels.copy())[np.newaxis, ...]
+                image_sizes = np.array([[img.shape[0], img.shape[1]]])
+            else:
+                add_resized = (channels.copy())[np.newaxis, ...]
+                pimages = np.concatenate((pimages, add_resized), axis=0)
+                image_sizes = np.concatenate((
+                    image_sizes,
+                    np.array([[img.shape[0], img.shape[1]]])
+                    ), axis=0)
+            channels = None
+
+        pimages = cv2.normalize(
+            pimages, None, alpha=0, beta=1,
+            norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F
+            )
+        return (pimages, image_sizes)
+
+    def show_boxes(self, image, boxes, box_classes, box_scores, file_name):
+        """
+           Displays the image with all boundary boxes,
+             class names, and box scores.
+
+           Args:
+            image: a numpy.ndarray containing an unprocessed image
+            boxes: a numpy.ndarray containing the boundary
+              boxes for the image
+            box_classes: a numpy.ndarray containing the class indices
+              for each box
+            box_scores: a numpy.ndarray containing the box scores
+              for each box
+            file_name: the file path where the original image is stored
+
+           Return:
+            None
+        """
+        BLUE = (255, 0, 0)
+        RED = (0, 0, 200)
+
+        for i, box in enumerate(boxes):
+            x, y = round(box[0]), round(box[1])
+            xWidth, yHeight = round(box[2]), round(box[3])
+
+            idd = box_classes[i]
+            score = " {:0.2f}".format(box_scores[i])
+            label = self.class_names[idd]+score
+            cv2.rectangle(
+                image, (int(x), int(y)),
+                (int(xWidth), int(yHeight)), BLUE, 2
+            )
+            cv2.putText(
+                image, label, (int(x-5), int(y-5)),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, RED, thickness=1,
+                lineType=cv2.LINE_AA
+                )
+
+        # cv2.imshow(file_name, image)
+        wait = input("Press the s button to save image: ")
+        if wait == "s":
+            import os
+            if os.path.isdir("./detections") is False:
+                os.mkdir("./detections")
+            cv2.imwrite("./detections/{}".format(file_name), image)
+
+        cv2.destroyAllWindows()
